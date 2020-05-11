c7f79f1dcc5d1e649fde44779b255668db7b1565
Jay Lux Ferro <jayluxferro@sperixlabs.org>
2020-05-11 15:36:13 +0000

    bug fixes

--- config/env/production.js
+++ config/env/production.js
@@ -371,9 +371,8 @@ module.exports = {
   *                                                                         *
   ***************************************************************************/
   custom: {
-    baseUrl: 'https://example.com',
-    internalEmailAddress: 'support@example.com',
-
+    //baseUrl: 'https://example.com',
+    //internalEmailAddress: 'support@example.com',
     // mailgunDomain: 'mg.example.com',
     // mailgunSecret: 'key-prod_fake_bd32301385130a0bafe030c',
     // stripeSecret: 'sk_prod__fake_Nfgh82401348jaDa3lkZ0d9Hm',
