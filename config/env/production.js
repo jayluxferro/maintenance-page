04224e62edc3a81af18a2a39992382269667bc8d
Jay Lux Ferro <jayluxferro@sperixlabs.org>
2020-05-11 15:50:56 +0000

    bug fixes

--- config/env/production.js
+++ config/env/production.js
@@ -250,10 +250,10 @@ module.exports = {
     * > Be sure to use the right protocol!  ("http://" vs. "https://")         *
     *                                                                          *
     ***************************************************************************/
-    // onlyAllowOrigins: [
-    //   'https://example.com',
-    //   'https://staging.example.com',
-    // ],
+     onlyAllowOrigins: [
+       'https://example.com',
+       'http://example.com
+     ],
 
 
     /***************************************************************************
